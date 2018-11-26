fa4796cdb989c114c1610de483c572d8b9730b52
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-11-26 18:01:01 -0500

    Mumuki Export on 2018-11-26 18:01:01 -0500

--- 00006_¡Bienvenido!/test..js
+++ 00006_¡Bienvenido!/test..js
@@ -8,7 +8,7 @@
   
   true.should.eql(hayUltimoPrompt, "<b><u>¿Llamaste a la función prompt?</u></b>")
   
-  document.querySelector("h1").innerHTML.should.be.eql('Bienvenido ' + _prompt_response_);  
+  document.querySelector("h1").nodeValue.should.be.eql('Bienvenido ' + _prompt_response_);  
 });
 
 it("Saludar Ale", function() {
@@ -20,7 +20,7 @@ it("Saludar Ale", function() {
   
   true.should.eql(hayUltimoPrompt, "<b><u>¿Llamaste a la función prompt?</u></b>")
   
-  document.querySelector("h1").innerHTML.should.be.eql('Bienvenido ' + _prompt_response_);  
+  document.querySelector("h1").nodeValue.should.be.eql('Bienvenido ' + _prompt_response_);  
 });
 /*#tests>*/
 /*<options#*/output_ignore_scripts: true
